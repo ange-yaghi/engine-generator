c927aaf7f53ed4f5b0edb1fc6290501e667784e2
hackall360 <alex.schott2002@gmail.com>
2023-06-07 00:48:15 -0700

    Wrote functionallity for v style engine

--- engine.py
+++ engine.py
@@ -38,6 +38,13 @@ def generate_firing_order_v(cylinders):
             right_bank.append(i)
         else:
             left_bank.append(i)
+
+    extra_cylinders = cylinders % 2
+    if extra_cylinders == 1:
+        if len(left_bank) > len(right_bank):
+            right_bank.append(cylinders)
+        else:
+            left_bank.append(cylinders)
     
     return left_bank + right_bank, left_bank, right_bank
 
@@ -68,6 +75,27 @@ def generate_inline(cylinderCount):
 
 def generate_v(cylinderCount):
     print("Generating V style engine...")
+    cylinders, cylinders0, cylinders1 = generate_firing_order_v(cylinderCount)
+
+    bank0 = engine_generator.Bank(cylinders0, -45)
+    bank1 = engine_generator.Bank(cylinders1, 45)
+
+    engine = engine_generator.Engine([bank0, bank1], cylinders)
+    
+    engine.engine_name = input("Engine name: ")
+    engine.starter_torque = int(input("Starter torque: "))
+    engine.crank_mass = int(input("Crank mass: "))
+    engine.bore = float(input("Cylinder bore: "))
+    engine.stroke = float(input("Cylinder stroke: "))
+    engine.chamber_volume = int(input("Chamber volume: "))
+    engine.rod_length = float(input("Cylinder rod length: "))
+    engine.simulation_frequency = int(input("Simulation frequency (default 1200): "))
+    engine.max_sle_solver_steps = int(input("Max sle solver steps (default 4): "))
+    engine.fluid_simulation_steps = int(input("Fluid simulation steps (default 4): "))
+    engine.idle_throttle_plate_position = float(input("Idle throttle plate position: "))
+
+    engine.generate()
+    engine.write_to_file(strip_special_characters(engine.engine_name) + ".mr")
 
 def generate_custom_engine():
     print("Generating custom engine...")
@@ -88,57 +116,5 @@ def generate_custom_engine():
     elif style.lower() == "v":
         generate_v(cylinderCount)
 
-def generate_v24():
-    cylinders0 = []
-    cylinders1 = []
-    cylinders = []
-
-    for i in range(12):
-        cylinders0.append(i * 2)
-        cylinders1.append(i * 2 + 1)
-        cylinders += [i * 2, i * 2 + 1]
-
-    b0 = engine_generator.Bank(cylinders0, -45)
-    b1 = engine_generator.Bank(cylinders1, 45)
-    engine = engine_generator.Engine([b0, b1], cylinders)
-    engine.engine_name = "V24"
-    engine.starter_torque = 400
-    engine.crank_mass = 200
-
-    engine.generate()
-    engine.write_to_file("test.mr")
-
-def generate_v69():
-    cylinders0 = []
-    cylinders1 = []
-    cylinders = []
-
-    for i in range(34):
-        cylinders0.append(i * 2)
-        cylinders1.append(i * 2 + 1)
-        cylinders += [i * 2, i * 2 + 1]
-
-    cylinders0.append(68)
-    cylinders.append(68)
-
-    b0 = engine_generator.Bank(cylinders0, -34.5)
-    b1 = engine_generator.Bank(cylinders1, 34.5)
-    b1.flip = True
-    engine = engine_generator.Engine([b0, b1], cylinders)
-    engine.engine_name = "V69"
-    engine.starter_torque = 10000
-    engine.crank_mass = 2000
-    engine.bore = 197.9
-    engine.stroke = 197.9
-    engine.chamber_volume = 3000
-    engine.rod_length = engine.stroke * 1.75
-    engine.simulation_frequency = 1200
-    engine.max_sle_solver_steps = 4
-    engine.fluid_simulation_steps = 4
-    engine.idle_throttle_plate_position = 0.9
-
-    engine.generate()
-    engine.write_to_file("v69_engine.mr")
-
 if __name__ == "__main__":
-    generate_i4()
+    generate_custom_engine()
